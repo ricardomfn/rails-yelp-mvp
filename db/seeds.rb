b4e56c3a75bdbe7d1f245565db43d862c31e2b26
ricardo martins <ricardomfneves@gmail.com>
2023-05-24 15:01:15 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: 'Resto de Chacal', address: '18 Rue Chacal', phone_number: "22323231231", category: 'chinese')
+Restaurant.create(name: 'Chacalus Maximus', address: '18 One Two Three',phone_number: "00304343", category: 'chinese')
+Restaurant.create(name: 'Marabout', address: '18 Rue Maraboutage', phone_number: "4343562", category: 'chinese')
+Restaurant.create(name: 'Végéta', address: '18 Rue Saiyan',phone_number: "231232132", category: 'chinese')
+Restaurant.create(name: 'Arsenal', address: '18 Rue on a perdu le titre',phone_number: "566234230", category: 'chinese')
